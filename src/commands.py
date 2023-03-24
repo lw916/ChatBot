15f460c68879f9466c37938a954d654182cbafcc
LiWei <22401237@life.hkbu.edu.hk>
2023-03-25 00:12:39 +0800

    add personal message pushing function

--- src/commands.py
+++ src/commands.py
@@ -1,10 +1,12 @@
 from telegram.ext import Updater, CommandHandler
 import configparser
 from db.db import redis_connect
+import requests
 import logging
 import redis
 
-global redis
+global redis_connection
+global config_content
 
 
 # main function of starting telegram bot
@@ -14,15 +16,16 @@ def bot():
     """
 
     # read the config file
-    config = configparser.ConfigParser()
-    config.read(r"../conf/config.ini")  # read config file from config.ini
+    global config_content
+    config_content = configparser.ConfigParser()
+    config_content.read(r"../conf/config.ini")  # read config file from config.ini
 
     # connect redis database
-    global redis
-    redis = redis_connect()
+    global redis_connection
+    redis_connection = redis_connect()
 
     # set the telegram bot token
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    updater = Updater(token=(config_content['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -31,11 +34,54 @@ def bot():
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("hello", hello))
+    dispatcher.add_handler(CommandHandler("push", push))
     dispatcher.add_handler(CommandHandler("add", add))
 
     # Start the telegram bot
     updater.start_polling()
     updater.idle()
+
+
+# push message to other user
+# example: https://api.telegram.org/bot6[botID]/sendMessage?chat_id=[userID]&text=[text message]
+def send(user_ID, message):
+    """
+    This is a function for bot to transfer message to other user who also subscribe the bot.
+    """
+
+    try:
+        requests.get("https://api.telegram.org/bot"
+                     + str(config_content['TELEGRAM']['ACCESS_TOKEN'])
+                     + "/sendMessage?chat_id="
+                     + str(user_ID)
+                     + "&text="
+                     + str(message)
+                     )
+        return True
+    except (IndexError, ValueError):
+        return False
+
+
+# test message pushing
+def push(update, context):
+    """
+    This is a command for testing the function of pushing the message to other user
+    """
+    try:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="I have receive your require and try to push message to your friends!")
+        if send(update.effective_chat["id"], "This is a testing message for "
+                                             + update.effective_chat["last_name"]
+                                             + ' '
+                                             + update.effective_chat["first_name"]
+                                             + "!"):
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Your message might successfully push to your friend.")
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Your message might fail to push to your friend.")
+    except (IndexError, ValueError):
+        update.message.reply_text('So sorry, I have a mistake of pushing message to your friends.')
 
 
 # start the bot
@@ -91,8 +137,8 @@ def add(update, context):
     """
     try:
         msg = context.args[0]
-        redis.incr(msg)
+        redis_connection.incr(msg)
         update.message.reply_text('You have said ' + msg + ' for ' +
-                                  redis.get(msg).decode('UTF-8') + ' times.')
+                                  redis_connection.get(msg).decode('UTF-8') + ' times.')
     except (IndexError, ValueError):
         update.message.reply_text('Oh right, your command might be wrong. This command usage: /add <keyword>')
