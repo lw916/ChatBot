28c561bef0a6203978afa1b66bf7f7331540eb9d
LiWei <22401237@life.hkbu.edu.hk>
2023-03-25 23:34:19 +0800

    add some functions

--- db/db.py
+++ db/db.py
@@ -10,4 +10,34 @@ def redis_connect():
     config = configparser.ConfigParser()
     config.read(r"../conf/config.ini")  # read config file from config.ini
     return redis.Redis(host=(config['DATABASE']['HOST']), password=(config['DATABASE']['PASSWORD']),
-                       port=(config['DATABASE']['PORT']))
+                       port=(config['DATABASE']['PORT']), decode_responses=True)
+
+
+# storing userID with userName
+def user_storing(redis_connection, userName, userID):
+    """
+    This is a function when user start to use the chatbot,
+    bot would record the user's id and its name for other functions.
+    """
+    try:
+        # for finding the user if the user in the database and avoid repeat record the userID
+        if redis_connection.get(userName):
+            return True
+        else:
+            redis_connection.set(userName, userID)
+            return True
+
+    except (IndexError, ValueError):
+        return False
+
+
+# database fuzzy query
+def fuzzy_query(redis_connection, query):
+    """
+    This is the function is for fuzzy query the database.
+    """
+    try:
+        keys = redis_connection.scan(match=query + "*")
+        return keys
+    except (IndexError, ValueError):
+        return []
--- src/commands.py
+++ src/commands.py
@@ -1,13 +1,17 @@
-from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
 import configparser
-from db.db import redis_connect
+from db.db import redis_connect, user_storing, fuzzy_query
 import requests
 import logging
 
 global redis_connection
 global config_content
 
+SEARCH = range(2)
 
+
+# main function part
 # main function of starting telegram bot
 def bot():
     """
@@ -16,7 +20,7 @@ def bot():
 
     # read the config file
     global config_content
-    config_content = configparser.ConfigParser()
+    config_content = configparser.ConfigParser()  # import ConfigParser to read the config file
     config_content.read(r"../conf/config.ini")  # read config file from config.ini
 
     # connect redis database
@@ -27,21 +31,184 @@ def bot():
     updater = Updater(token=(config_content['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
 
+    # setup for logging on the terminal
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
     # add different command into the telegram bot
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("hello", hello))
-    dispatcher.add_handler(CommandHandler("push", push))
-    dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("start", start))  # inject start command
+    dispatcher.add_handler(CallbackQueryHandler(start_menu_actions))  # inject start menu function handler
+    dispatcher.add_handler(search_conversation_handler)  # inject search conversation handler
+    dispatcher.add_handler(CommandHandler("help", help_command))  # inject help command
+    dispatcher.add_handler(CommandHandler("hello", hello))  # inject greet command
+    dispatcher.add_handler(CommandHandler("push", push))  # inject push message to others command
+    # @TODO database function will be remove in the future
+    dispatcher.add_handler(CommandHandler("add", add))  # inject database testing command
+    dispatcher.add_handler(CommandHandler("search", search_user))  # inject search user command
 
     # Start the telegram bot
     updater.start_polling()
     updater.idle()
+# main function part end
 
 
-# push message to other user
+# start command
+# start the bot
+def start(update, context):
+    """
+    This function is used to start the bot chat.
+    This is the first time user subscribe the bot and the bot's reply!
+    """
+    try:
+        # Main menu edit/design
+        keyboard = [[InlineKeyboardButton('Chat with bot', callback_data='hello')],
+                    [InlineKeyboardButton('Bot help', callback_data='help')],
+                    [InlineKeyboardButton('Push message', callback_data='push')]]
+        start_menu_keyboard = InlineKeyboardMarkup(keyboard)
+        userName = update.effective_chat["last_name"] + update.effective_chat["first_name"]
+        user_storing(redis_connection, userName, update.effective_chat["id"])
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 reply_markup=start_menu_keyboard,
+                                 text="Hi! " + userName
+                                      + ',\n'
+                                      + 'I am telegram bot provide movie recommending and sharing for you.\n'
+                                      + 'Hope you could get your best result here!\n'
+                                      + 'Please use the bot command to chat with me!',
+
+                                 )
+    except (IndexError, ValueError):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Oh right, your command might be wrong. This command usage is: /start')
+
+
+# main menu callback reply
+def start_menu_actions(update, context):
+    query = update.callback_query
+    if query.data == 'hello':
+        hello(update, context)
+    elif query.data == 'help':
+        help_command(update, context)
+    elif query.data == 'push':
+        push(update, context)
+    else:
+        update.message.reply_text('Please select the right command!')
+
+
+# start command part end.
+
+
+# greeting command part start
+# Greeting user
+def hello(update, context):
+    """
+    This code is used to greeting user.
+    When user use /hello would return user's message:
+    Good day, user.
+    """
+    try:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Good day, " + update.effective_chat["last_name"] + ' ' +
+                                      update.effective_chat["first_name"])
+    except (IndexError, ValueError):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Oh right, your command might be wrong. This command usage is: /hello')
+
+
+# greet part end
+
+
+# Help command
+def help_command(update, context):
+    """
+    This command is provided user with this bot help.
+    Like provide some command example for user.
+    """
+    try:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Hello! ' + update.effective_chat["last_name"] + ' ' +
+                                      update.effective_chat["first_name"] + '. This is a helping message.')
+    except (IndexError, ValueError):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Oh right, your command might be wrong. This command usage is: /help')
+
+
+# Help part end
+
+
+# Database calculate
+def add(update, context):
+    """
+    THis command is provided to test redis services
+    """
+    try:
+        msg = context.args[0]
+        redis_connection.incr(msg)
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='You have said ' + msg + ' for ' +
+                                      redis_connection.get(msg).decode('UTF-8') + ' times.')
+    except (IndexError, ValueError):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Oh right, your command might be wrong. This command usage: /add <keyword>')
+
+
+# Database part end
+
+
+# This part is for user searching question
+# For searching user's question
+def search_user_question(update, context):
+    """
+    When press the start button and use the search button
+    bot would ask the user for its name
+    """
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Please enter the username for searching the user.')
+    return SEARCH
+
+
+# search for the user
+def search_user(update, context):
+    """
+    This function is the command for searching the user.
+    """
+    try:
+        match_word_from_question = update.message.text
+        keys = fuzzy_query(redis_connection, match_word_from_question)
+        if not keys[1]:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Can not find the user, please check your match key")
+            return ConversationHandler.END
+        else:
+            friend_list = ""
+            for i in keys[1]:
+                friend_list += i + "\n"
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text="Friend List:\n" + friend_list + "Please check the list if someone is your "
+                                                                           "friends.")
+            return ConversationHandler.END
+    except (IndexError, ValueError):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Sorry, your command might be wrong.Usage: /search <keyword>. Please try again.')
+        return ConversationHandler.END
+
+
+def search_cancel(update, context):
+    update.message.reply_text('Search Conversation cancelled.')
+    return ConversationHandler.END
+
+
+search_conversation_handler = ConversationHandler(
+    entry_points=[CommandHandler('search', search_user_question)],
+    states={
+        SEARCH: [MessageHandler(Filters.text & ~Filters.command, search_user)],
+    },
+    fallbacks=[CommandHandler('cancel', search_cancel)],
+)
+
+
+# search part end
+
+
+# push message to other user part
 # example: https://api.telegram.org/bot6[botID]/sendMessage?chat_id=[userID]&text=[text message]
 def send(user_ID, message):
     """
@@ -80,64 +247,6 @@ def push(update, context):
             context.bot.send_message(chat_id=update.effective_chat.id,
                                      text="Your message might fail to push to your friend.")
     except (IndexError, ValueError):
-        update.message.reply_text('So sorry, I have a mistake of pushing message to your friends.')
-
-
-# start the bot
-def start(update, context):
-    """
-    This function is used to start the bot chat.
-    This is the first time user subscribe the bot and the bot's reply!
-    """
-    try:
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Hi! " + update.effective_chat["last_name"] + ' ' + update.effective_chat["first_name"]
-                                      + ',\n'
-                                      + 'I am telegram bot provide movie recommending and sharing for you.\n'
-                                      + 'Hope you could get your best result here!\n'
-                                      + 'Please use the bot command to chat with me!'
-                                 )
-    except (IndexError, ValueError):
-        update.message.reply_text('Oh right, your command might be wrong. This command usage is: /start')
-
-
-# Greeting user
-def hello(update, context):
-    """
-    This code is used to greeting user.
-    When user use /hello would return user's message:
-    Good day, user.
-    """
-    try:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="Good day, " + update.effective_chat["last_name"] + ' ' +
-                                      update.effective_chat["first_name"])
-    except (IndexError, ValueError):
-        update.message.reply_text('Oh right, your command might be wrong. This command usage is: /hello')
-
-
-# Help command
-def help_command(update, context):
-    """
-    This command is provided user with this bot help.
-    Like provide some command example for user.
-    """
-    try:
-        update.message.reply_text('Hello! ' + update.effective_chat["last_name"] + ' ' +
-                                  update.effective_chat["first_name"] + '. This is a helping message.')
-    except (IndexError, ValueError):
-        update.message.reply_text('Oh right, your command might be wrong. This command usage is: /help')
-
-
-# Database calculate
-def add(update, context):
-    """
-    THis command is provided to test redis services
-    """
-    try:
-        msg = context.args[0]
-        redis_connection.incr(msg)
-        update.message.reply_text('You have said ' + msg + ' for ' +
-                                  redis_connection.get(msg).decode('UTF-8') + ' times.')
-    except (IndexError, ValueError):
-        update.message.reply_text('Oh right, your command might be wrong. This command usage: /add <keyword>')
+                                 text='So sorry, I have a mistake of pushing message to your friends.')
+# push data to others part end
